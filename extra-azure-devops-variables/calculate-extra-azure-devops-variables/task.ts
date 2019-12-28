c2ed2b9f435434c01d18d507283f37e64fde28b5
Floris Devreese <florisdevreese@hotmail.com>
2019-12-28 16:42:59 +0100

    add logic for saving variable

--- extra-azure-devops-variables/calculate-extra-azure-devops-variables/task.ts
+++ extra-azure-devops-variables/calculate-extra-azure-devops-variables/task.ts
@@ -1,34 +1,45 @@
 import tl = require('azure-pipelines-task-lib/task')
+import tr = require('azure-pipelines-task-lib/toolrunner');
 
+// ------------------------------------ prerequisites ------------------------------------
+if (process.env.SYSTEM_ACCESSTOKEN == null) {
+    tl.setResult(tl.TaskResult.Failed, "No access to SYSTEM_ACCESSTOKEN. You must provide access to System.AccessToken. See how: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#systemaccesstoken")
+    process.exit(-1)
+}
+
+let supportedProviders = ["TfsGit", "Git", "GitHub"]
+if (!supportedProviders.includes(<string>process.env.BUILD_REPOSITORY_PROVIDER)) {
+    tl.setResult(tl.TaskResult.Failed, `This task only supports git based version control systems like: ${supportedProviders.join(', ')}. '${process.env.BUILD_REPOSITORY_PROVIDER}' is not supported.`)
+    process.exit(-1)
+}
+
+// ------------------------------------ functions ------------------------------------
+function getActiveSprintName() : string {
+    console.debug("Calculate active sprint")
+    console.debug(`Get commit time of ${process.env.BUILD_SOURCEVERSION}`)
+
+    
+    return "testValue"
+
+}
+
+// ------------------------------------ program flow ------------------------------------
 try {
-    // ------------------------------------ prerequisites ------------------------------------
-    if (process.env.SYSTEM_ACCESSTOKEN == null) {
-        tl.setResult(tl.TaskResult.Failed, "No access to SYSTEM_ACCESSTOKEN. You must provide access to System.AccessToken. See how: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#systemaccesstoken")
-        process.exit(-1)
+    let extraVariables: { [variable: string]: string } = {}
+    extraVariables["EXTRAVARIABLES_ACTIVESPRINT"] = getActiveSprintName()
+
+    console.log("Variables calculated:")
+    console.log(extraVariables)
+
+    for(let variable in extraVariables) {
+        tl.setVariable(variable, extraVariables[variable])
     }
-
-    let supportedProviders = ["TfsGit", "Git", "GitHub"]
-    if (!supportedProviders.includes(<string>process.env.BUILD_REPOSITORY_PROVIDER)) {
-        tl.setResult(tl.TaskResult.Failed, `This task only supports git based version control systems like: ${supportedProviders.join(', ')}. '${process.env.BUILD_REPOSITORY_PROVIDER}' is not supported.`)
-        process.exit(-1)
-    }
-    // ------------------------------------ functions ------------------------------------
-
-    // ------------------------------------ program flow ------------------------------------
-
-    let variabele = process.env.PATH
-
-
-    console.log(`path variable: ${variabele}`)
-
-
-    // const inputString: string | undefined = tl.getInput('samplestring', true)
-    // if (inputString == 'bad') {
-    //     tl.setResult(tl.TaskResult.Failed, 'Bad input was given')
-    //     return
-    // }
-    // console.log('Hello', inputString)
 }
 catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message)
 }
+
+
+
+// todo
+//  - check if process.exit(-1) is necessary running this script inside a task
