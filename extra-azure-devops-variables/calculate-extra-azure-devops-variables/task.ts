0ee6a9e9778bb5913304284588800a02717d1c68
Floris Devreese <florisdevreese@hotmail.com>
2019-12-28 15:09:40 +0100

    rebuild app for typescript

--- extra-azure-devops-variables/calculate-extra-azure-devops-variables/task.ts
+++ extra-azure-devops-variables/calculate-extra-azure-devops-variables/task.ts
@@ -1,17 +1,28 @@
 import tl = require('azure-pipelines-task-lib/task');
 
-async function run() {
-    try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
-        }
-        console.log('Hello', inputString);
+try {
+    // ------------------------------------ prerequisites ------------------------------------
+    if (process.env.SYSTEM_ACCESSTOKEN == null) {
+        tl.setResult(tl.TaskResult.Failed, "No access to SYSTEM_ACCESSTOKEN. You must provide access to System.AccessToken in order for this task to work.");
     }
-    catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
-    }
-}
 
-run();
+    // ------------------------------------ functions ------------------------------------
+
+    // ------------------------------------ program flow ------------------------------------
+
+    let variabele = process.env.path;
+
+
+    console.log(`path variable: ${variabele}`);
+
+
+    // const inputString: string | undefined = tl.getInput('samplestring', true);
+    // if (inputString == 'bad') {
+    //     tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+    //     return;
+    // }
+    // console.log('Hello', inputString);
+}
+catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+}
