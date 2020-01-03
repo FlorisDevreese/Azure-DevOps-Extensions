a5313b5a31f4ae1230e4d213eeaf7e49145e821e
Floris Devreese <florisdevreese@hotmail.com>
2020-01-03 20:35:14 +0100

    #218 make it work when used inside a release

--- calculate-extra-azure-devops-variables/task/task.ts
+++ calculate-extra-azure-devops-variables/task/task.ts
@@ -7,25 +7,35 @@ if (process.env.SYSTEM_ACCESSTOKEN == null) {
     tl.setResult(tl.TaskResult.Failed, "No access to SYSTEM_ACCESSTOKEN. You must provide access to System.AccessToken. See how: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#systemaccesstoken")
 }
 
-let supportedProviders = ["TfsGit", "Git", "GitHub"]
-if (!supportedProviders.includes(<string>process.env.BUILD_REPOSITORY_PROVIDER)) {
-    tl.setResult(tl.TaskResult.Failed, `This task only supports git based version control systems like: ${supportedProviders.join(', ')}. '${process.env.BUILD_REPOSITORY_PROVIDER}' is not supported.`)
-}
-
 // ------------------------------------ functions ------------------------------------
 async function getActiveSprintName(): Promise<string> {
     console.log("Calculate active sprint")
-    tl.debug(`Get commit time of ${process.env.BUILD_SOURCEVERSION}`)
-    let gitResponse: tr.IExecSyncResult = tl.execSync("git", `show -s --format=%ci ${process.env.BUILD_SOURCEVERSION}`)
-    if (!Date.parse(gitResponse.stdout)) {
-        tl.warning(`Cannot calculate the active sprint because it could not get the commit time of commit '${process.env.BUILD_SOURCEVERSION}'. This can be the result of the "Don't sync sources" option that is enabled.`)
-        return ""
-    }
-    let commitTime: Date = new Date(`${gitResponse.stdout}`)
-    tl.debug(`Time of commit: ${commitTime}`)
 
-    let commitDay = new Date(Date.UTC(commitTime.getFullYear(), commitTime.getMonth(), commitTime.getDate()))
-    tl.debug(`Commit day in UTC (used for comparing dates): ${commitDay}`)
+    // todo if it's an release, then get the current time
+    let compareTime: Date = new Date()
+    if(process.env.BUILD_SOURCEVERSION != null){
+        tl.debug(`Check if a Git based version control system is used.`)
+        let supportedProviders = ["TfsGit", "Git", "GitHub"]
+        if (!supportedProviders.includes(<string>process.env.BUILD_REPOSITORY_PROVIDER)) {
+            tl.setResult(tl.TaskResult.Failed, `This task only supports git based version control systems like: ${supportedProviders.join(', ')}. '${process.env.BUILD_REPOSITORY_PROVIDER}' is not supported.`)
+        }
+
+        tl.debug(`Get commit time of ${process.env.BUILD_SOURCEVERSION}`)
+        let gitResponse: tr.IExecSyncResult = tl.execSync("git", `show -s --format=%ci ${process.env.BUILD_SOURCEVERSION}`)
+        if (!Date.parse(gitResponse.stdout)) {
+            tl.warning(`Cannot calculate the active sprint because it could not get the commit time of commit '${process.env.BUILD_SOURCEVERSION}'. This can be the result of the "Don't sync sources" option that is enabled.`)
+            return ""
+        }
+        compareTime = new Date(`${gitResponse.stdout}`)
+        tl.debug(`Time of commit: ${compareTime}`)
+    }
+    else {
+        tl.debug(`No BUILD_SOURCEVERSION available. Will use the current time current time for calculating the active sprint.`)
+        compareTime = new Date()
+    }
+
+    let compareDay = new Date(Date.UTC(compareTime.getFullYear(), compareTime.getMonth(), compareTime.getDate()))
+    tl.debug(`Start of the day in UTC (used for comparing dates): ${compareDay}`)
 
     tl.debug("Get all sprints")
     var options = {
@@ -42,7 +52,7 @@ async function getActiveSprintName(): Promise<string> {
     allSprints.forEach(function (sprint) { tl.debug(` -  ${sprint.name}: ${sprint.attributes.startDate} -> ${sprint.attributes.finishDate}`) })
 
     tl.debug(`Get active sprints at time of commit`)
-    let activeSprints: Array<any> = allSprints.filter(sprint => commitDay >= new Date(sprint.attributes.startDate) && commitDay < new Date(sprint.attributes.finishDate))
+    let activeSprints: Array<any> = allSprints.filter(sprint => compareDay >= new Date(sprint.attributes.startDate) && compareDay < new Date(sprint.attributes.finishDate))
     tl.debug(`Active sprints: `)
     activeSprints.forEach(function (sprint) { tl.debug(` -  ${sprint.name}: ${sprint.attributes.startDate} -> ${sprint.attributes.finishDate}`) })
 
